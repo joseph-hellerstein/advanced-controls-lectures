6b64558b83ce648d717138b89281a36e147bed35
Joseph Hellerstein <jlheller@uw.edu>
2024-02-06 07:37:49 -0800

    Exploring compensation

--- activate.sh
+++ activate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source setup_run.sh
-PYTHONPATH=`pwd`/src:${PYTHONPATH}
+PYTHONPATH=`pwd`/src:${PYTHONPATH}:/Users/jlheller/home/Technical/repos/controlSBML/src
 source adv/bin/activate
